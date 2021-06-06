f8c9bdfabee4c2d498c527cd6d06a151641a94e3
olawaleKareemDev <olawalekareemdev@gmail.com>
2021-06-06 18:56:22 +0100

    first python_refresher_commit

---
+++ overview.py
@@ -0,0 +1,18 @@
+
+#  empty line spaces before variable declaration really matters in python
+
+
+# Data types in python
+# 1.
+# using the typeof all variables must be declared except that of a complex number 
+# the coefficient oof the complex variable must be used.
+
+a = 5
+b = 6.0
+m = 1-1j
+
+print (a, "is type of ", type(a))
+print (b, "is type of ", type(b))
+print(m,isinstance(1-1j, complex))
+
+
