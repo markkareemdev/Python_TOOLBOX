d266d832cbd7a253193ae3a4ea1861a994b418cf
olawaleKareemDev <olawalekareemdev@gmail.com>
2021-06-07 02:55:03 +0100

    scond commit to the python refresher course

--- overview.py
+++ overview.py
@@ -1,18 +1,52 @@
 
 #  empty line spaces before variable declaration really matters in python
+#  in python there is no variable declaration like in js.. where yo have let,var and const type of variables respectively
 
 
 # Data types in python
 # 1.
+# number data types
 # using the typeof all variables must be declared except that of a complex number 
 # the coefficient oof the complex variable must be used.
 
-a = 5
-b = 6.0
-m = 1-1j
+# a = 5
+# b = 6.0
+# m = 1-1j
 
-print (a, "is type of ", type(a))
-print (b, "is type of ", type(b))
-print(m,isinstance(1-1j, complex))
+# print (a, "is type of ", type(a))
+# print (b, "is type of ", type(b))
+# print(m,isinstance(1-1j, complex))
+
+#2.
+# List data types
+# list = [1,2,3,4,5]
+# print(list[2])
+# using the slicing operator
+# print(list[0:4]) 
+# changing list item
+# list[4] = 6;
+# print(list)
+
+# 3
+# Tuple data types
+# an ordered sequence of items just as a list
+# unlike list tuples are immutable once created.
+# it values can be extracted using the slicing operator, []
+# it is defined with parenthesis ()
+# t = (5, "program", 1+4j)
+# print(t[1])
 
 
+#4
+#python strings
+# a slicing operator can be used here to
+# space is also countd as a string here
+# strings are also immutable
+
+# s = "This is a string."
+# p = ''' A multiLine
+#         string'''
+# print(s)
+# print(p)
+
+# print(s[4:18])
